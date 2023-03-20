a07a76f3f6228c0428bd746439c315df1699ad6d
Akash <jaimin@webbrainstechnologies.com>
2023-03-20 16:16:35 +0530

    property schema changed

--- models/propertyschema.js
+++ models/propertyschema.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const propertySchema = new mongoose.Schema({tracks_details: [{ type: mongoose.Schema.Types.ObjectId, ref: 'tracks' }]}, { strict: false });
+const propertySchema = new mongoose.Schema({tracks_details: { type: mongoose.Schema.Types.ObjectId, ref: 'tracks' }}, { strict: false });
 
 const PropertySchema = mongoose.model("property", propertySchema);
 
