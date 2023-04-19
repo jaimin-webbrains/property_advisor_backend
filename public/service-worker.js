14bad7777d8872b01edb5d36c0e48669789eace3
jaimin <jaimin@webbrainstechnologies.com>
2023-04-19 18:42:22 +0530

    front build

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ed5f9546c65b7dfc34a4efe1dbaa6f01.js"
+  "/precache-manifest.4d9c9c179233d51983d4646ee5224a10.js"
 );
 
 self.addEventListener('message', (event) => {
